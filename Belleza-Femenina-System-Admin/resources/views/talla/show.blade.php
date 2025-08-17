bd6f1310963da6114b56455e0be85ca452caf4e0
SrCroy <rubiocr007@gmail.com>
2025-08-17 17:38:45 -0600

    Agregando estilos en show.blade.php de la seccion de tallas y termiando de dar estilos a toda la seccion de tallas

--- Belleza-Femenina-System-Admin/resources/views/talla/show.blade.php
+++ Belleza-Femenina-System-Admin/resources/views/talla/show.blade.php
@@ -4,31 +4,33 @@
     {{ $talla->name ?? __('Show') . " " . __('Talla') }}
 @endsection
 
+@push('styles')
+<link rel="stylesheet" href="{{ url('/css/categorias/crearCategoria.css') }}">
+@endpush
+
 @section('content')
     <section class="content container-fluid">
         <div class="row">
             <div class="col-md-12">
-                <div class="card">
-                    <div class="card-header" style="display: flex; justify-content: space-between; align-items: center;">
+                <div class="card custom-card">
+                    <div class="card-header custom-card-header" style="display: flex; justify-content: space-between; align-items: center;">
                         <div class="float-left">
                             <span class="card-title">{{ __('Show') }} Talla</span>
                         </div>
                         <div class="float-right">
-                            <a class="btn btn-primary btn-sm" href="{{ route('tallas.index') }}"> {{ __('Back') }}</a>
+                            <a class="btn btn-submit btn-sm" href="{{ route('tallas.index') }}"> {{ __('Back') }}</a>
                         </div>
                     </div>
 
-                    <div class="card-body bg-white">
-                        
-                                <div class="form-group mb-2 mb20">
-                                    <strong>Id Talla:</strong>
-                                    {{ $talla->id_talla }}
-                                </div>
-                                <div class="form-group mb-2 mb20">
-                                    <strong>Talla:</strong>
-                                    {{ $talla->talla }}
-                                </div>
-
+                    <div class="card-body bg-white custom-card-body">
+                        <div class="form-group mb-2 mb20">
+                            <strong>Id Talla:</strong>
+                            {{ $talla->id_talla }}
+                        </div>
+                        <div class="form-group mb-2 mb20">
+                            <strong>Talla:</strong>
+                            {{ $talla->talla }}
+                        </div>
                     </div>
                 </div>
             </div>
