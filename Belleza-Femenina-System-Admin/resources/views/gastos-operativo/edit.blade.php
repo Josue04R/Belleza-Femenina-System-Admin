2e9cfdae2f6664ac577b0cf672028b180efa0589
SrCroy <rubiocr007@gmail.com>
2025-08-18 07:30:24 -0600

    Agregando estilos en edit.blade.php en la seccion de gastos operativos

--- Belleza-Femenina-System-Admin/resources/views/gastos-operativo/edit.blade.php
+++ Belleza-Femenina-System-Admin/resources/views/gastos-operativo/edit.blade.php
@@ -4,17 +4,23 @@
     {{ __('Update') }} Gastos Operativo
 @endsection
 
-@section('content')
-    <section class="content container-fluid">
-        <div class="">
-            <div class="col-md-12">
+@push('styles')
+<link rel="stylesheet" href="{{ url('/css/categorias/crearCategoria.css') }}">
+<link rel="stylesheet" href="{{ url('/css/tablas/tablas.css') }}">
+@endpush
 
-                <div class="card card-default">
-                    <div class="card-header">
+@section('content')
+    <br>
+    <br>
+    <section class="content container-fluid">
+        <div class="row mx-1">
+            <div class="col-12 px-2">
+                <div class="card shadow-sm border-0 custom-card mb-4">
+                    <div class="card-header custom-card-header py-3 px-4">
                         <span class="card-title">{{ __('Update') }} Gastos Operativo</span>
                     </div>
-                    <div class="card-body bg-white">
-                        <form method="POST" action="{{ route('gastos-operativos.update', $gastosOperativo->id) }}"  role="form" enctype="multipart/form-data">
+                    <div class="card-body bg-white px-4 py-3">
+                        <form method="POST" action="{{ route('gastos-operativos.update', $gastosOperativo->id) }}" class="form-edit" role="form" enctype="multipart/form-data">
                             {{ method_field('PATCH') }}
                             @csrf
 
