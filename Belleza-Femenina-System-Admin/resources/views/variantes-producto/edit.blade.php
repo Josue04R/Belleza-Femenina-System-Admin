1ae5b4a33ffbb19e2ce1ebaedd8a5daa7f2548b1
SrCroy <rubiocr007@gmail.com>
2025-08-18 06:55:50 -0600

    Agregando estilo en edit.blade.php en la seccion de variantes-producto

--- Belleza-Femenina-System-Admin/resources/views/variantes-producto/edit.blade.php
+++ Belleza-Femenina-System-Admin/resources/views/variantes-producto/edit.blade.php
@@ -4,7 +4,13 @@
     {{ __('Update') }} Variantes Producto
 @endsection
 
+@push('styles')
+<link rel="stylesheet" href="{{ url('/css/categorias/crearCategoria.css') }}">
+@endpush
+
 @section('content')
+    <br>
+    <br>
     <section class="content container-fluid">
         <div class="">
             <div class="col-md-12">
@@ -14,7 +20,7 @@
                         <span class="card-title">{{ __('Update') }} Variantes Producto</span>
                     </div>
                     <div class="card-body bg-white">
-                        <form method="POST" action="{{ route('variantes-productos.update', $variantesProducto->id_variantes) }}"  role="form" enctype="multipart/form-data">
+                        <form method="POST" action="{{ route('variantes-productos.update', $variantesProducto->id_variantes) }}" class="form-edit" role="form" enctype="multipart/form-data">
                             {{ method_field('PATCH') }}
                             @csrf
 
